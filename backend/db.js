634cbf7e5d2f7f732f543b99b96c48ecf102c9df
phamela <phamelafuentess@gmail.com>
2026-05-23 19:43:16 -0500

    Configurando variables para Railway

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,14 @@
 // db.js
+require('dotenv').config();
+
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host:     'localhost',
-  port:     3306,
-  database: 'BeautyShop',
-  user:     'root',
-  password: 'root18',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 const db = pool.promise();
